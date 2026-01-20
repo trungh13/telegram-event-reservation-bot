dbc4c5fc4dd35eade579a6d03cbd377a91554cf5
trung <htrung1311@yahoo.com>
2026-01-20 10:18:18 +0200

    feat(telegram): import primitives modules into TelegramModule

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -3,9 +3,15 @@ import { TelegramService } from './telegram.service';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegramController } from './telegram.controller';
+import { AccountModule } from '../account/account.module';
+import { EventModule } from '../event/event.module';
+import { ParticipationModule } from '../participation/participation.module';
 
 @Module({
   imports: [
+    AccountModule,
+    EventModule,
+    ParticipationModule,
     TelegrafModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
