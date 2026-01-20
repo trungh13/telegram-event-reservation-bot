ccdea8de987ad39d1f9ece6a15a6cc55cf56e8d2
trung <htrung1311@yahoo.com>
2026-01-20 12:23:26 +0200

    chore: maintenance - untrack ignored files and update app dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TelegramModule } from './telegram/telegram.module';
+import { AccountModule } from './account/account.module';
+import { EventModule } from './event/event.module';
+import { ParticipationModule } from './participation/participation.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { TelegramUserModule } from './telegram-user/telegram-user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     TelegramModule,
+    AccountModule,
+    EventModule,
+    ParticipationModule,
+    SchedulerModule,
+    TelegramUserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
