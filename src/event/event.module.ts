4445ff22276986d04334891de9f57c194537595d
trung <htrung1311@yahoo.com>
2026-01-22 13:00:38 +0200

    fix(module): resolve circular dependency between Scheduler and Event modules

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { EventService } from './event.service';
 import { SchedulerModule } from '../scheduler/scheduler.module';
 
 @Module({
-  imports: [SchedulerModule],
+  imports: [forwardRef(() => SchedulerModule)],
   providers: [EventService],
   exports: [EventService],
 })
--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TelegrafModule } from 'nestjs-telegraf';
+import { EventModule } from '../event/event.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     TelegrafModule,
+    forwardRef(() => EventModule),
   ],
   providers: [SchedulerService],
   exports: [SchedulerService],
